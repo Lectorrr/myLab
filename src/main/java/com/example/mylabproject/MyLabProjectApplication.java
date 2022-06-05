d8c539023e4fa71d4718d3b7b7558bd0b0ec4a30
Hsuehsihui <lectorhsueh@163.com>
2022-06-05 18:35:29 +0800

    1、创建spring boot项目

---
+++ src/main/java/com/example/mylabproject/MyLabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mylabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLabProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLabProjectApplication.class, args);
+    }
+
+}
